2cb856a0b1f06c42a9d3b02d4c40d4fdc3fa6f00
eightscrow <183341520+eightscrow@users.noreply.github.com>
2026-09-11 11:08:06 +0300

    enchilada: Adapt product configuration for VoltageOS

--- voltage_enchilada.mk
+++ voltage_enchilada.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from enchilada device
 $(call inherit-product, device/oneplus/enchilada/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS configuration.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_enchilada
+PRODUCT_NAME := voltage_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
